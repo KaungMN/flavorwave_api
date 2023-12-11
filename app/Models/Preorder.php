ae6a270b81d2615edf9d94d62f91938f6e5284b5
maung129 <awea60505@gmail.com>
2023-12-11 14:10:35 +0630

    data seeding

--- app/Models/Preorder.php
+++ app/Models/Preorder.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Preorder extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'product_id',
+        'box_pcs',
+        'slug',
+        'city',
+        'township',
+        'address',
+        'orderType',
+        'status',
+        'remark'
+    ];
+
+    //one to many
+    public function customer(){
+        return $this->belongsTo(Customer::class);
+    }
+
+    //many to many
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
 }
