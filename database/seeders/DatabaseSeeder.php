cb275f7a09596b9b05884a5ce58ac50a2d5d9aec
Thiri <thiri7301@gmail.com>
2023-12-11 14:23:56 +0630

    no message

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Supplier;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,6 +22,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             ProductSeeder::class,
+            SupplierSeeder::class,
+            DepartmentSeeder::class
         ]);
     }
 }
