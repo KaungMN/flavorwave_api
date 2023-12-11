e44654b6f9c7179d24f4f433a3c3b18d40852d8e
KyawMyoHtet22 <myohtetk109@gmail.com>
2023-12-11 14:05:39 +0630

    table_relation

---
+++ database/seeders/DepartmentSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Department;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DepartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $departments = [
+            ['name' => 'Admin'],
+            ['name' => 'Sales'],
+            ['name' => 'Logistics'],
+            ['name' => 'Warehouse'],
+            ['name' => 'Factory'],
+        ];
+
+
+        foreach ($departments as $d) {
+            Department::create([
+                'slug' => uniqid(),
+                'name' => $d['name'],
+            ]);
+        }
+    }
+}
